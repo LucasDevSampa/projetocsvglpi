7b79104bf82bbbe642d32c48a3ee9e52a38505e5
Lucas Chaves <lucasc789@gmail.com>
2021-10-08 23:48:53 -0300

    Realizado a organização do codigo principal e funçoes

--- TrataArquivoExcel.py
+++ TrataArquivoExcel.py
@@ -8,44 +8,27 @@ def TratarArquivo(arquivo):
     ws1 = wb['Chamados Atribuidos']
     
     ws1['A1'].value = 'Quantidade'
+    print(ws1.max_row)
 
-    
-    #Intera entre as celulas de uma coluna
-    """i = 1
-    while i <= ws1.max_row:
-        print(ws1.cell(i,2).value)
-        i = i + 1"""
-
-    #Filtra a Coluna Entidade
+    #Trecho que trata a Coluna entidade
     i = 2
-    while i != None:
+    while i <= ws1.max_row:
         text = ws1['D' + str(i)].value.split("-")
-        ws1['D' + str(i)].value = text[1]    
-        i+=1
-        
-    """
+        ws1['D' + str(i)].value = text[1]
+        i = i + 1
+
+    #Trecho que trata a numeração da quantidade
     i = 2
     while i <= ws1.max_row:
-        print(ws1['D' + str(i)].value)
-        i+=1"""
-
-    #print()
-    #criar filtro
-    ws1.auto_filter.ref = "A1:D1"
+        ws1['A' + str(i)].value += 1
+        i = i + 1
+    
 
     #Salvar A planilha
     wb.save("FieldSP.xlsx")
 
 
-    #Intera entre as celulas de uma coluna versao 2
-    """
-    i = 2
-    while i <= ws1.max_row:
-        print(ws1['B' + str(i)].value)
-        #wb.create_sheet(ws1['B' + str(i)].value)
-        i+=1"""
-
-
+    
 
 
 
